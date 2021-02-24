3939d9f7a15917ba0e8e6e5ee164822c1bd69857
neige-i <ibrahim.naji.rachdi@gmail.com>
2021-02-24 18:17:18 +0100

    Handle opening hours in list view

---
+++ app/src/main/java/com/neige_i/go4lunch/view/list/PlaceHourWrapper.java
@@ -0,0 +1,33 @@
+package com.neige_i.go4lunch.view.list;
+
+import androidx.annotation.IdRes;
+import androidx.annotation.NonNull;
+
+class PlaceHourWrapper {
+
+    @NonNull
+    private final String hours;
+    @IdRes
+    private final int fontColor;
+    @IdRes
+    private final int fontStyle;
+
+    PlaceHourWrapper(@NonNull String hours, int fontColor, int fontStyle) {
+        this.hours = hours;
+        this.fontColor = fontColor;
+        this.fontStyle = fontStyle;
+    }
+
+    @NonNull
+    public String getHours() {
+        return hours;
+    }
+
+    public int getFontColor() {
+        return fontColor;
+    }
+
+    public int getFontStyle() {
+        return fontStyle;
+    }
+}
