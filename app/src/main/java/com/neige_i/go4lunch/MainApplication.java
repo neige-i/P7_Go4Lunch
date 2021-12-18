73e4642052d700fda881b139efb4693f291642ce
neige-i <ibrahim.naji.rachdi@gmail.com>
2021-12-18 16:59:04 +0100

    Set WorkRequest from OneTime to Periodic

--- app/src/main/java/com/neige_i/go4lunch/MainApplication.java
+++ app/src/main/java/com/neige_i/go4lunch/MainApplication.java
@@ -5,6 +5,15 @@ import android.app.Application;
 import androidx.annotation.NonNull;
 import androidx.hilt.work.HiltWorkerFactory;
 import androidx.work.Configuration;
+import androidx.work.PeriodicWorkRequest;
+import androidx.work.WorkManager;
+
+import com.neige_i.go4lunch.background.NotifyTimeToEatWorker;
+
+import java.time.Clock;
+import java.time.Duration;
+import java.time.LocalTime;
+import java.util.concurrent.TimeUnit;
 
 import javax.inject.Inject;
 
@@ -14,7 +23,7 @@ import dagger.hilt.android.HiltAndroidApp;
 public class MainApplication extends Application implements Configuration.Provider {
 
     @Inject
-    WorkerDelegate workerDelegate;
+    Clock clock;
     @Inject
     HiltWorkerFactory hiltWorkerFactory;
 
@@ -22,7 +31,21 @@ public class MainApplication extends Application implements Configuration.Provid
     public void onCreate() {
         super.onCreate();
 
-        workerDelegate.enqueueOneTimeWorkRequest(this);
+        WorkManager.getInstance(this).enqueue(
+            new PeriodicWorkRequest.Builder(NotifyTimeToEatWorker.class, 1, TimeUnit.DAYS)
+                .setInitialDelay(getTimeDiffToMiddayInMillis(), TimeUnit.MILLISECONDS)
+                .build()
+        );
+    }
+
+    private long getTimeDiffToMiddayInMillis() {
+        Duration timeDiff = Duration.between(LocalTime.now(clock), LocalTime.of(12, 0));
+
+        if (timeDiff.isNegative()) {
+            timeDiff = timeDiff.plusDays(1);
+        }
+
+        return timeDiff.toMillis();
     }
 
     @NonNull
